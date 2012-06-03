c0dd2ad435d3d3ed74f809bc85878250f6b2c496
Will Fleming <wfleming77@gmail.com>
2012-06-03 16:24:12 -0700

    Bump cpmojure version to 1.0.4

--- web-compojure/project.clj
+++ web-compojure/project.clj
@@ -1,7 +1,7 @@
 (defproject web-compojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [compojure "1.0.1"]
+                 [compojure "1.0.4"]
                  [hiccup "0.3.8"]
                  [cheshire "2.1.0"]]
   :dev-dependencies [[lein-ring "0.5.4"]]
