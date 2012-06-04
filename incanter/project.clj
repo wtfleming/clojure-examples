aad1d2dfe88f8af52f2bdb27b4b02cffaf19b1b5
Will Fleming <wfleming77@gmail.com>
2012-06-03 19:19:07 -0700

    Incanter example

---
+++ incanter/project.clj
@@ -0,0 +1,6 @@
+(defproject incanterexample "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [
+    [org.clojure/clojure "1.3.0"]
+    [incanter "1.3.0"]]
+  :main incanterexample.core)
---
+++ incanter/src/incanterexample/core.clj
@@ -0,0 +1,9 @@
+(ns incanterexample.core
+  (:gen-class)
+  (:use (incanter core stats charts)))
+
+
+(defn -main []
+  (view (histogram (sample-normal 1000)))
+)
+
