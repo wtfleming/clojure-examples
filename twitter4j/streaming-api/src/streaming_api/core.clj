bba04035eba32717bdf7a424ed4d1462fafac581
Will Fleming <wfleming77@gmail.com>
2012-06-09 13:44:47 -0700

    Cleanup

--- twitter4j/streaming-api/src/streaming_api/core.clj
+++ twitter4j/streaming-api/src/streaming_api/core.clj
@@ -1,33 +1,23 @@
 (ns streaming-api.core
   (:gen-class)
   (:import [twitter4j
-              StatusListener
-              TwitterStream TwitterStreamFactory
+              StatusListener TwitterStream TwitterStreamFactory
               FilterQuery]
-           [twitter4j.conf ConfigurationBuilder]))
+           [twitter4j.conf ConfigurationBuilder Configuration]))
 
-;(defn foo []
-;(proxy [StatusListener] []
-;  (onStatus [status] (println status))
-;)
-;)
-(set! *warn-on-reflection* true)
-
-(defn -main []
+;(set! *warn-on-reflection* true)
 
 
-(defn config []
+(defn config-with-password ^Configuration []
+  "Build a twitter4j configuration object with a username/password pair"
   (let [cb (ConfigurationBuilder.)]
-    (.setUser cb "USERNAME")
-    (.setPassword cb "PASSWORD")
+    (.setUser cb "YOUR-USERNAME")
+    (.setPassword cb "YOUR-PASSWORD")
     (.build cb)))
 
 
-;(def filter-query (FilterQuery. 0, (long-array []), (into-array String ["clojure"])))
-(def filter-query (FilterQuery. 0, (long-array []), (into-array String ["twitter"])))
-
-
 (defn status-listener []
+  "Implementation of twitter4j's StatusListener interface"
   (proxy [StatusListener] []
     (onStatus [^twitter4j.Status status] (println (.getText status)))
     (onException [^java.lang.Exception e] (.printStackTrace e))
@@ -36,73 +26,41 @@
     (onTrackLimitationNotice [numberOfLimitedStatuses] ())))
 
 
-;(def aaa (.getFilterStream (TwitterStreamFactory. (config)) filter-query))
-
-(def bbb (.getInstance (TwitterStreamFactory. (config))))
-(def b (.getFilterStream bbb filter-query))
-
+(defn get-twitter-stream-factory[]
+  (let [factory (TwitterStreamFactory. (config-with-password))]
+    (.getInstance factory)))
 
 
+(defn do-filter-stream []
+  ; We want tweets with the word tweet in them
+  (let [filter-query (FilterQuery. 0 (long-array []) (into-array String ["tweet"]))
+        stream (.getFilterStream (get-twitter-stream-factory) filter-query)]
+    (while true
+      (.next stream (status-listener)))))
 
 
-(while true
-  (.next b (status-listener)))
+(defn do-sample-stream []
+  (let [stream (get-twitter-stream-factory)]
+    (.addListener stream (status-listener))
+    (.sample stream)))
 
 
-
-
-
-;
-;
-;(def bbb (.getInstance (TwitterStreamFactory. (config))))
-;(.addListener bbb (status-listener))
-;(.sample bbb)
-
-  
+(defn -main []
+  (do-filter-stream)
+;  (do-sample-stream)
+ 
 )
 
 
 
-;(.addListener bbb (proxy [StatusListener] []
-;  (onStatus [^twitter4j.Status status] (println (.getText status)))
-;  (onException [^java.lang.Exception e] (.printStackTrace e))
-;  (onDeletionNotice [^twitter4j.StatusDeletionNotice statusDeletionNotice] ())
-;  (onScrubGeo [userId upToStatusId] ())
-;  (onTrackLimitationNotice [numberOfLimitedStatuses] ())))
-
-
+; In event want to use OAuth, here's the java
 ;ConfigurationBuilder cb = new ConfigurationBuilder();
-;cb.setDebugEnabled(true)
+;  cb.setDebugEnabled(true)
 ;  .setOAuthConsumerKey("*********************")
 ;  .setOAuthConsumerSecret("******************************************")
 ;  .setOAuthAccessToken("**************************************************")
 ;  .setOAuthAccessTokenSecret("******************************************");
 ;TwitterFactory tf = new TwitterFactory(cb.build());
-;Twitter twitter = tf.getInstance();
 
 
-;(proxy [baseclass-name] [baseclass-constructor-parameters]
-;    method-redefinition-1
-;    ...
-;    method-redefinition-N)
-;where a method redefinition looks like:
-;
-;(method-name [parameters]
-;    method-body)
 
-
-;public static void main(String[] args) throws TwitterException, IOException{
-;    StatusListener listener = new StatusListener(){
-;        public void onStatus(Status status) {
-;            System.out.println(status.getUser().getName() + " : " + status.getText());
-;        }
-;        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-;        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-;        public void onException(Exception ex) {
-;            ex.printStackTrace();
-;        }
-;    };
-;    TwitterStream twitterStream = new TwitterStreamFactory(listener).getInstance();
-;    // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods ;continuously.
-;    twitterStream.sample();
-;}
