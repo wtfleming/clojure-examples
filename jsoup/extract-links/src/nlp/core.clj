4b0a29c395a9c33f9ea3e232e9b2957b1de4e165
Will Fleming <wfleming77@gmail.com>
2012-07-01 15:30:02 -0700

    Add Jsoup example

---
+++ jsoup/extract-links/src/nlp/core.clj
@@ -0,0 +1,10 @@
+(ns nlp.core
+  (:import (java.io FileInputStream))
+  (:import (org.jsoup Jsoup)))
+
+  
+(defn -main []
+  (with-open [input (FileInputStream. "en.wikipedia.org.html")]
+    (let [doc (Jsoup/parse input "UTF-8" "http://en.wikipedia.org/wiki/Main_Page")
+          links (.select doc "a[href]")]
+      (doseq [link links] (println (.attr link "abs:href"))))))
