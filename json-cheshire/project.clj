8af608214a72b3e6767c48660eccae2d879d2bc7
Will Fleming <wfleming77@gmail.com>
2012-07-12 19:10:01 -0700

    Update cheshire to version 4.0.1

--- json-cheshire/project.clj
+++ json-cheshire/project.clj
@@ -1,5 +1,5 @@
 (defproject json "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cheshire "4.0.0"]]
+                 [cheshire "4.0.1"]]
   :main json.core)
