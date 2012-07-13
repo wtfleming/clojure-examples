e972b37c60df0f8672c2ed864dc5eca8d70de5b2
Will Fleming <wfleming77@gmail.com>
2012-07-12 20:00:57 -0700

    Working websocket example

--- websockets/webbit-example/project.clj
+++ websockets/webbit-example/project.clj
@@ -1,3 +1,8 @@
 (defproject webbit-example "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.webbitserver/webbit "0.4.12"]
+                 [cheshire "4.0.1"]]
+  :main webbit-example.core)
+
+
--- websockets/webbit-example/src/webbit_example/core.clj
+++ websockets/webbit-example/src/webbit_example/core.clj
@@ -1,1 +1,23 @@
-(ns webbit-example.core)
+(ns webbit-example.core
+  (:require [cheshire.core :as json]
+            [clojure.string :as s])
+  (:import [org.webbitserver WebServer WebServers WebSocketHandler]
+           [org.webbitserver.handler StaticFileHandler]))
+
+
+(defn on-message [connection json-message]
+  (let [m (json/parse-string json-message true)
+        {{message :message} :data} m]
+    (.send connection (json/generate-string
+                       {:type "upcased" :message (s/upper-case message)}))))
+
+(defn -main []
+  (doto (WebServers/createWebServer 8080)
+    (.add "/websocket"
+          (proxy [WebSocketHandler] []
+            (onOpen [c] (prn "opened" c))
+            (onClose [c] (prn "closed" c))
+            (onMessage [c j] (on-message c j))))
+    (.add (StaticFileHandler. "."))
+    (.start)))
+
