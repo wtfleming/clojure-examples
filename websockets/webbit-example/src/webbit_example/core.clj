4e9f03ae3c6d1de669b6b62d93443d15aa1e49bb
Will Fleming <wfleming77@gmail.com>
2012-07-12 19:10:28 -0700

    Initial commit of websockets example

---
+++ websockets/webbit-example/src/webbit_example/core.clj
@@ -0,0 +1,1 @@
+(ns webbit-example.core)
---
+++ websockets/webbit-example/test/webbit_example/test/core.clj
@@ -0,0 +1,6 @@
+(ns webbit-example.test.core
+  (:use [webbit-example.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
