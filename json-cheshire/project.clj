d50706ad12f8024ba6c9fefaf78639c78dec99c7
Will Fleming <wfleming77@gmail.com>
2013-06-08 14:21:01 -0700

    Update cheshire version

--- json-cheshire/project.clj
+++ json-cheshire/project.clj
@@ -1,5 +1,5 @@
 (defproject json "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cheshire "4.0.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.2.0"]]
   :main json.core)
--- web-compojure/project.clj
+++ web-compojure/project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.0"]
-                 [cheshire "4.0.0"]]
+                 [cheshire "5.2.0"]]
   :dev-dependencies [[ring/ring-devel "1.1.0"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler web-compojure.core/app})
