14190dd701ec5de40631d8072b01abcb22ae3634
Will Fleming <wfleming77@gmail.com>
2013-06-08 14:14:48 -0700

    Update clojure to 1.5, and dependencies. Use lein 2

--- web-compojure/project.clj
+++ web-compojure/project.clj
@@ -1,8 +1,9 @@
 (defproject web-compojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [compojure "1.0.4"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
                  [hiccup "1.0.0"]
                  [cheshire "4.0.0"]]
-  :dev-dependencies [[lein-ring "0.7.0"]]
+  :dev-dependencies [[ring/ring-devel "1.1.0"]]
+  :plugins [[lein-ring "0.8.3"]]
   :ring {:handler web-compojure.core/app})
