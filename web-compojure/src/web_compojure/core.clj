673a9937bf22622c556fe1f5ba242b5063e9bd96
Will Fleming <wfleming77@gmail.com>
2013-06-17 20:25:37 -0700

    Create function to handle json responses

--- web-compojure/src/web_compojure/core.clj
+++ web-compojure/src/web_compojure/core.clj
@@ -1,20 +1,22 @@
 (ns web-compojure.core
   (:use [compojure.core]
-        [hiccup.core]
-        [cheshire.core])
+        [hiccup.core])
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [cheshire.core :as json]))
+
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
+
 
 (defroutes main-routes
   (GET "/" [] "<h1>Hello World Wide Web!</h1>")
   (GET "/hola" [] (html [:h1 "Hola World"]))
   (GET "/hola/:id" [id]
     (str "<h1>Hi " id "</h1>"))
-
-  (GET "/hola.json" [] {:status 200
-                        :headers {"Content-Type" "application/json"}
-                        :body (generate-string {:message "hello world wide web"})})
-
+  (GET "/hola.json" [] (json-response {:message "hello world wide web!"}))
 
   (route/resources "/")
   (route/not-found "Page not found"))
