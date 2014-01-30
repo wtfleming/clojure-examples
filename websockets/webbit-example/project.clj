6c2e39736041a79cfae1d5d025ba906e6778a5d5
Will Fleming <wfleming77@gmail.com>
2014-01-29 20:18:57 -0800

    update webbit version

--- websockets/webbit-example/project.clj
+++ websockets/webbit-example/project.clj
@@ -1,8 +1,8 @@
 (defproject webbit-example "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.webbitserver/webbit "0.4.12"]
-                 [cheshire "4.0.1"]]
+                 [org.webbitserver/webbit "0.4.15"]
+                 [cheshire "5.3.1"]]
   :main webbit-example.core)
 
 
