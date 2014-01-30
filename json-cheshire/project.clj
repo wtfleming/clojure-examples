1548d2a17efd325b75c92d17d3f1f60122b9b878
Will Fleming <wfleming77@gmail.com>
2014-01-29 20:04:53 -0800

    Update cheshire dependency to 5.3.1

--- json-cheshire/project.clj
+++ json-cheshire/project.clj
@@ -1,5 +1,5 @@
 (defproject json "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.2.0"]]
+                 [cheshire "5.3.1"]]
   :main json.core)
