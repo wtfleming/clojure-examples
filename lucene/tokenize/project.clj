04c3c6af3a1c1631f39eef438f288a4846dae6bb
Will Fleming <will@ubermedia.com>
2014-10-03 17:50:46 -0700

    update clojure to 1.6.0

--- lucene/tokenize/project.clj
+++ lucene/tokenize/project.clj
@@ -1,6 +1,6 @@
 (defproject lucene "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-    [org.clojure/clojure "1.5.1"]
+    [org.clojure/clojure "1.6.0"]
     [org.apache.lucene/lucene-core "3.6.0"]]
   :main lucene.core)
