baa2ed45c7973856df0915f87623e77debe91a72
Will Fleming <wfleming77@gmail.com>
2014-12-22 19:22:43 -0800

    change formatting

--- jsoup/extract-links/src/nlp/core.clj
+++ jsoup/extract-links/src/nlp/core.clj
@@ -2,9 +2,9 @@
   (:import (java.io FileInputStream))
   (:import (org.jsoup Jsoup)))
 
-
 (defn -main []
   (with-open [input (FileInputStream. "en.wikipedia.org.html")]
     (let [doc (Jsoup/parse input "UTF-8" "http://en.wikipedia.org/wiki/Main_Page")
           links (.select doc "a[href]")]
-      (doseq [link links] (println (.attr link "abs:href"))))))
+      (doseq [link links]
+        (println (.attr link "abs:href"))))))
