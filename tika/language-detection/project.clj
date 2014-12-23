a5d325511e5669b8c827c3adf9e0f78be422842f
Will Fleming <wfleming77@gmail.com>
2014-12-22 19:27:18 -0800

    update dependencies

--- tika/language-detection/project.clj
+++ tika/language-detection/project.clj
@@ -1,6 +1,6 @@
 (defproject tika "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-    [org.clojure/clojure "1.5.1"]
-    [org.apache.tika/tika-core "1.4"]]
+    [org.clojure/clojure "1.6.0"]
+    [org.apache.tika/tika-core "1.6"]]
   :main tika.core)
