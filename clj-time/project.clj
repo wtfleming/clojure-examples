931b871d375a81acbf391cc69e4dd4844bad1b55
Will Fleming <wfleming77@gmail.com>
2014-12-22 17:43:02 -0800

    update dependencies

--- clj-time/project.clj
+++ clj-time/project.clj
@@ -1,5 +1,5 @@
 (defproject json "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.8.0"]]
   :main time.core)
