e195b610d79c0e2cc3f258338d25c3214e22748e
Will Fleming <wfleming77@gmail.com>
2014-12-22 19:19:11 -0800

    update dependencies

--- jsoup/extract-links/project.clj
+++ jsoup/extract-links/project.clj
@@ -1,5 +1,5 @@
 (defproject extract-links "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.jsoup/jsoup "1.7.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.jsoup/jsoup "1.8.1"]]
   :main nlp.core)
