9e16c68bca6b9966e55ca412ec7565e63e7c2925
Will Fleming <wfleming77@gmail.com>
2021-07-12 19:49:11 -0400

    update opennlp example

--- opennlp/sentence-detection/project.clj
+++ opennlp/sentence-detection/project.clj
@@ -1,5 +1,5 @@
 (defproject sentence-detection "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.opennlp/opennlp-tools "1.5.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.apache.opennlp/opennlp-tools "1.9.3"]]
   :main nlp.core)
