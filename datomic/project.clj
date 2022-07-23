95d49b567120808e4e1eeb6f8c0474d562b54b19
Will Fleming <wfleming77@gmail.com>
2022-07-23 14:10:51 -0400

    fix project.clj datomic config

--- datomic/project.clj
+++ datomic/project.clj
@@ -1,8 +1,8 @@
 (defproject datomic "0.1.0-SNAPSHOT"
   :description "Examples of working with Datomic"
   :repositories [["cognitect-dev-tools" {:url      "https://dev-tools.cognitect.com/maven/releases/"
-                                         :datomic-username :env
-                                         :datomic-password :env}]]
+                                         :username :env
+                                         :password :env}]]
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
